f3d8d9edf1dcd141753f1bb54b858a3834897484
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-09-21 21:27:56 -0300

    Radio - Implementing FadeInOut Option For The PlayMusic Function

--- Assets/Radio/Scripts/Radio.cs
+++ Assets/Radio/Scripts/Radio.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Radio : MonoBehaviour
@@ -11,6 +12,8 @@ public class Radio : MonoBehaviour
     [SerializeField] private SoundLibrary musicLibrary;
     [SerializeField] private SoundLibrary sfxLibrary;
 
+    private Coroutine currentFade;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -22,17 +25,79 @@ public class Radio : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void PlayMusic(string id)
+    // MUSICS METHODS //
+
+    public void PlayMusic(string id, bool fade = false, float fadeDuration = 1f)
     {
         var sound = musicLibrary.GetSound(id);
         if (sound == null || sound.clip == null) return;
 
+        if (fade)
+        {
+            if (currentFade != null) StopCoroutine(currentFade);
+            currentFade = StartCoroutine(FadeInMusic(sound, fadeDuration));
+        }
+        else
+        {
+            musicSource.Stop();
+            musicSource.clip = sound.clip;
+            musicSource.loop = sound.loop;
+            musicSource.volume = sound.volume;
+            musicSource.Play();
+        }
+    }
+
+    public void StopMusic(bool fade = false, float fadeDuration = 1f)
+    {
+        if (fade)
+        {
+            if (currentFade != null) StopCoroutine(currentFade);
+            currentFade = StartCoroutine(FadeOutMusic(fadeDuration));
+        }
+        else
+        {
+            musicSource.Stop();
+        }
+    }
+
+    
+    private IEnumerator FadeInMusic(SoundData sound, float duration)
+    {
         musicSource.Stop();
         musicSource.clip = sound.clip;
         musicSource.loop = sound.loop;
-        musicSource.volume = sound.volume;
+        musicSource.volume = 0f;
         musicSource.Play();
+
+        float timer = 0f;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float progress = duration > 0f ? timer / duration : 1f;
+            musicSource.volume = Mathf.Lerp(0f, sound.volume, progress);
+            yield return null;
+        }
+        musicSource.volume = sound.volume;
     }
+
+    private IEnumerator FadeOutMusic(float duration)
+    {
+        float startVolume = musicSource.volume;
+
+        float timer = 0f;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float progress = duration > 0f ? timer / duration : 1f;
+            musicSource.volume = Mathf.Lerp(startVolume, 0f, progress);
+            yield return null;
+        }
+
+        musicSource.Stop();
+        musicSource.volume = startVolume;
+    }
+
+    // SFX METHODS
 
     public void PlaySFX(string id)
     {
@@ -52,10 +117,5 @@ public class Radio : MonoBehaviour
         {
             Destroy(temp, sound.clip.length + 0.1f);
         }
-    }
-
-    public void StopMusic()
-    {
-        musicSource.Stop();
     }
 }
--- Assets/Radio/Scripts/RadioInputTester.cs
+++ Assets/Radio/Scripts/RadioInputTester.cs
@@ -5,8 +5,10 @@ public class RadioInputTester : MonoBehaviour
     [Header("Teclas de Debug")]
     [SerializeField] private KeyCode musicKey = KeyCode.M;
     [SerializeField] private KeyCode sfxKey = KeyCode.N;
+    [SerializeField] private KeyCode musicFadeIn = KeyCode.I;
+    [SerializeField] private KeyCode musicFadeOut = KeyCode.O;
 
-    private bool isMusicPlaying = false;
+    [SerializeField] private bool isMusicPlaying = false;
 
     private void Update()
     {
@@ -22,6 +24,18 @@ public class RadioInputTester : MonoBehaviour
                 Radio.Instance.PlayMusic("Music/Test01");
                 isMusicPlaying = true;
             }
+        }
+
+        if (Input.GetKeyDown(musicFadeIn))
+        {
+            Radio.Instance.PlayMusic("Music/Test01", true, 1f);
+            isMusicPlaying = true;
+        }
+
+        if (Input.GetKeyDown(musicFadeOut))
+        {
+            Radio.Instance.StopMusic(true, 1f);
+            isMusicPlaying = false;
         }
 
         if (Input.GetKeyDown(sfxKey))
